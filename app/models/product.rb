278b269e90c9d0e67b9cade20966d2581b5357e3
htchuc26 <thanhchuc.cnttk38@gmail.com>
2021-06-16 09:46:16 +0700

    Validate_data

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   has_many :rates, dependent: :destroy
   has_many :order_details, dependent: :destroy
   has_many :orders, through: :order_details, source: :order
+
+  validates :name, presence: true
+  validates :infor, presence: true
+  validates :price, presence: true,
+    numericality: {greater_than: Settings.number.greater_than}
+  validates :quantity, presence: true,
+    numericality: {only_integer: true,
+                   greater_than: Settings.number.greater_than}
 end
--- app/models/suggest.rb
+++ app/models/suggest.rb
@@ -2,4 +2,7 @@ class Suggest < ApplicationRecord
   belongs_to :user
 
   enum status: {waiting: 0, accepted: 1, deny: 2}
+
+  validates :name, presence: true
+  validates :infor, presence: true
 end
