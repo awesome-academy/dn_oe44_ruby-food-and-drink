f859b15e6ed61cb03578f7ef6581e664a73f6d5b
Huynh Thanh Chuc <thanhchuc.cnttk38@gmail.com>
2021-06-22 22:08:29 +0700

    Show_Product_Information

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,7 @@
 module ProductsHelper
   def product_image product
     if product.images.blank?
-      image_tag("image_default.jpg")
+      image_tag("image_default.jpg", alt: :default)
     else
       image_tag(product.images.first, class: "food")
     end
